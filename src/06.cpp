2bdd78c29e77cef670371af8849b2445be6fef1e
rad <rs.wasiak@gmail.com>
2023-01-23 10:29:09 +0100

    Day 6

--- src/06.cpp
+++ src/06.cpp
@@ -2,10 +2,8 @@
 #include <fstream>
 #include <string>
 
-int findStartMarker(std::ifstream &infile)
+int findPacketMarker(std::string &line)
 {
-    std::string line;
-    std::getline(infile, line);
     std::string sequence;
     int counter = 0;
 
@@ -30,12 +28,47 @@ int findStartMarker(std::ifstream &infile)
     return -1;
 }
 
+int findMessageMarker(std::string &line)
+{
+    std::string sequence;
+    int counter = 0;
+
+    for (int i = 0; i < line.length(); ++i) {
+        if (counter == 0) {
+            sequence = line[i];
+            counter++;
+            continue;
+        }
+        if (sequence.find(line[i]) != std::string::npos) {
+            std::string sequenceCopy = sequence;
+            for (int j = 0; j < sequence.length(); ++j) {
+                sequenceCopy.erase(0, 1);
+                if (sequenceCopy.find(line[i]) != std::string::npos) {
+                    continue;
+                }
+                sequence.erase(0, j + 1);
+                counter = (int)sequence.length();
+                break;
+            }
+        }
+        sequence += line[i];
+        counter++;
+
+        if (counter == 15) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
     std::ifstream infile;
     infile.open("input.txt");
-
-    std::cout << findStartMarker(infile) << std::endl;
-
+    std::string line;
+    std::getline(infile, line);
     infile.close();
+
+    std::cout << "Part one: " << findPacketMarker(line) << std::endl;
+    std::cout << "Part two: " << findMessageMarker(line) << std::endl;
 }
